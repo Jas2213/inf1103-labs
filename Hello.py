4e5a7386a089740b905eaa26b9273281c4cbf292
Jason <jasonteo1303@gmail.com>
2026-09-02 16:16:33 +0800

    Second Post

--- Hello.py
+++ Hello.py
@@ -1,2 +1,27 @@
 print("Hello world")
 
+username = "cool_creator"
+bio = "fun blogger"
+followers = 100
+
+followers += 50
+print("Day1:", followers)
+
+followers += 20
+print("Day2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
+
+
+
+
+
+print("username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+
+
+
+
